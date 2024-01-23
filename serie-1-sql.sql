ff34d9898eb861ef46ecfbe474f00eff9ffee227
root <root@ip-172-31-30-65.us-east-2.compute.internal>
2024-01-23 19:35:17 +0000

    serie-1-sql

---
+++ serie-1-sql.sql
@@ -0,0 +1,18 @@
+SELECT phone_number, last_name, Salary FROM employees;
+
+ SELECT * FROM jobs;
+
+SELECT employee_id, first_name, (12*salary)"ANNUAL SALARY" FROM employees;
+
+
+DESCRIBE departments;
+SELECT * FROM departments;
+
+DESCRIBE employees;
+SELECT employee_id, last_name, job_id, hire_date FROM employees;
+
+SELECT DISTINCT employee.job_id FROM employees;
+
+
+SELECT employee_id Emp,last_name Employee, job_id,hire_date "Hire Date" FROM employees;
+
